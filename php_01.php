f289d24e92fdef026915d8f68711f211c0a7e46a
nabhon <nabhon.k@gmail.com>
2024-12-19 15:25:49 +0700

    first commit
    i have no idea

---
+++ php_01.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container mt-5">
+        <?php $mul = 2; ?>
+            <h1>สูตรคูณแม่ <?php echo $mul; ?></h1>
+            <?php for($i=1;$i<=12;$i++) { ?>
+            <div class="row">
+                <div class="h2 col text-end"><?php echo $mul; ?> x <?php echo $i?> =</div>
+                <div class="h2 col text-start"><?php echo $mul*$i?></div>
+            </div>
+            <?php } ?>
+    </div>
+</body>
+</html>
